f153081ee515004dd023c6a73a0cac761eea372f
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-19 20:45:35 -0300

    tests fixes and libs update

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,12 +1,16 @@
-/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: ['./src/setupTests.ts'],
-    }
+  plugins: [
+    sveltekit(),
+    svelteTesting(),
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest-setup.ts'],
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
 });
