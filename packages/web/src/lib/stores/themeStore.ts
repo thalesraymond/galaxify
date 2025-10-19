bee275604f715a3b16eb9b8598a29bf65f654fe9
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-19 18:50:47 -0300

    fixes

--- packages/web/src/lib/stores/themeStore.ts
+++ packages/web/src/lib/stores/themeStore.ts
@@ -1,26 +1,26 @@
-import { writable } from 'svelte/store';
-import { browser } from '$app/environment';
+import { writable } from "svelte/store";
+import { browser } from "$app/environment";
 
 type Theme =
-  | 'catppuccin-dark'
-  | 'nord-dark'
-  | 'tokyo-night-dark'
-  | 'gruvbox-dark'
-  | 'catppuccin-latte'
-  | 'nord-light'
-  | 'tokyo-night-light'
-  | 'gruvbox-light';
+    | "catppuccin-dark"
+    | "nord-dark"
+    | "tokyo-night-dark"
+    | "gruvbox-dark"
+    | "catppuccin-latte"
+    | "nord-light"
+    | "tokyo-night-light"
+    | "gruvbox-light";
 
-const defaultTheme: Theme = 'catppuccin-dark';
-const initialTheme = browser ? window.localStorage.getItem('theme') as Theme || defaultTheme : defaultTheme;
+const defaultTheme: Theme = "catppuccin-dark";
+const initialTheme = browser ? (window.localStorage.getItem("theme") as Theme) || defaultTheme : defaultTheme;
 
-const theme = writable<Theme>(initialTheme);
+const theme = writable<string>(initialTheme);
 
 theme.subscribe((value) => {
-  if (browser) {
-    window.localStorage.setItem('theme', value);
-    document.body.setAttribute('data-theme', value);
-  }
+    if (browser) {
+        window.localStorage.setItem("theme", value);
+        document.body.setAttribute("data-theme", value);
+    }
 });
 
 export default theme;
