185999f69a4612fdb393a688f6bf075262f06d8c
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-19 15:48:51 -0300

    fix build error

--- packages/api/src/config/database.ts
+++ packages/api/src/config/database.ts
@@ -1,12 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  try {
-    const connStr = process.env.COSMO_DB_CONNECTION_STRING || 'mongodb://localhost:27017/galaxify';
-    await mongoose.connect(connStr);
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    try {
+        const connStr = process.env.COSMO_DB_CONNECTION_STRING || "mongodb://localhost:27017/galaxify";
+        await mongoose.connect(connStr);
+        console.log("MongoDB Connected...");
+    } catch (err) {
+        const error = err as Error;
+        console.error(error?.message);
+        process.exit(1);
+    }
 };
