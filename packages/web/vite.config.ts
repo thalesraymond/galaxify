fa762283d8a3a0503e8d9d7272e28ac60de07ac8
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-19 21:13:43 -0300

    update tsconfig to add types for testing

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vitest/config';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { svelteTesting } from '@testing-library/svelte/vite';
+import { defineConfig } from "vitest/config";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { svelteTesting } from "@testing-library/svelte/vite";
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    svelteTesting(),
-  ],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./vitest-setup.ts'],
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-  },
+    plugins: [sveltekit(), svelteTesting()],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: ["./vitest-setup.ts"],
+        include: ["src/**/*.{test,spec}.{js,ts}"],
+    },
 });
