ac1c5179336de2bd7daa3ef0f556ce82326df69b
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-20 10:56:32 -0300

    reorganize dependencies

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,20 +1,29 @@
-import express from 'express';
-import helmet from 'helmet';
-import mongoSanitize from 'express-mongo-sanitize';
-import morgan from 'morgan';
+import express from "express";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import morgan from "morgan";
 
 const app = express();
+
+app.use((req, res, next) => {
+    Object.defineProperty(req, "query", {
+        ...Object.getOwnPropertyDescriptor(req, "query"),
+        value: req.query,
+        writable: true,
+    });
+    next();
+});
 
 app.use(helmet());
 app.use(express.json());
 app.use(mongoSanitize());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
 }
 
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ status: 'ok' });
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
 });
 
 export default app;
