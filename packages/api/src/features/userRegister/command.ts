75e6055d773b854d193d81d55946c6cec262b04c
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-21 06:15:55 -0300

    :D

---
+++ packages/api/src/features/userRegister/command.ts
@@ -0,0 +1,27 @@
+// The core business logic and data access
+import { RegisterUserDto } from "@galaxify/commons";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+// The entity representing our user
+export interface User {
+    email: string;
+    passwordHash: string;
+}
+
+// All logic for the registration command is here
+export async function handleRegisterUserCommand(dto: RegisterUserDto): Promise<User> {
+    // 1. Business Logic: Hash the password
+    const passwordHash = await bcrypt.hash(dto.password, 10);
+
+    const newUser: User = {
+        email: dto.email,
+        passwordHash: passwordHash,
+    };
+
+    // 2. Data Access: Save the user to the database
+    // TODO: Create a models folder in ./src/models to define UserModel using mongoose
+
+    console.log(`[Vertical Slice] Saved user ${newUser}`);
+    return newUser; // TODO: retrieve user created with id from database
+}
