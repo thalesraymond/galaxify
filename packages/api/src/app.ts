9d73b28ad6847d0a33120dcb6bc463c6ed0d9d2f
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-22 22:40:58 -0300

    Revert "feat(api): add custom error classes (#27)"
    This reverts commit 05faced804775ff392218e7f1d19beab40769348.

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -2,8 +2,24 @@ import express from "express";
 import helmet from "helmet";
 import mongoSanitize from "express-mongo-sanitize";
 import morgan from "morgan";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// routes
+
+import authRouter from "./features/userRegister/index.js";
+
+// end routes
+
+// middleware
+
+import limiter from "./middleware/rateLimiter.js";
+
+// end middleware
 
 const app = express();
+
+dotenv.config();
 
 app.use((req, res, next) => {
     Object.defineProperty(req, "query", {
@@ -22,8 +38,28 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
+app.use("/api", limiter);
+
 app.get("/api/health", (req, res) => {
     res.status(200).json({ status: "ok" });
 });
+
+app.use("/api/auth", authRouter);
+
+try {
+    if (!process.env.MONGO_CONNECTION_STRING) {
+        throw new Error("MONGO_CONNECTION_STRING is not defined");
+    }
+
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+        dbName: "galaxify",
+    });
+
+    app.listen(process.env.PORT ?? 5100, () => {
+        console.log("server running....");
+    });
+} catch (error) {
+    console.log(error);
+}
 
 export default app;
