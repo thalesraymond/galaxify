1ce79b6530175c57367535b16a9c3020236f8048
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-23 17:50:42 -0300

    remove wrong config

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     turbopack: {
         root: path.resolve("../../"),
     },
+    rewrites: async () => {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://localhost:4000/api/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
